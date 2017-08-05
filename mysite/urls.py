d4c36da07852f180619bbb310853c0bb102b2e27
brbuxton <brbuxton@cisco.com>
2017-08-05 11:22:09 -0400

    8/4/17 adjusting entrypoint

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^polls/', include('polls.urls')),
+    #url(r'^polls/', include('polls.urls')),
     url(r'^apic/', include('apic_em.urls')),
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
 ]
